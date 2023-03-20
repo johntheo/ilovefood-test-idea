af01a274c52e5debb27c305d213d895215368cdd
John Theo <johntheo@Johns-MacBook-Pro.local>
2023-03-20 11:34:58 -0300

    adding chrome as a dependency

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,32 @@
 const functions = require("firebase-functions");
-const playwright = require("playwright");
+const {chromium} = require("playwright-core");
+const bundledChromium = require("chrome-aws-lambda");
 
-exports.scrapePage = functions.https.onRequest(async (req, res) => {
-  const url = req.query.url;
+exports.scrapePage = functions.runWith({timeoutSeconds: 300, memory: "1GB"})
+    .https.onRequest(async (req, res) => {
+      const url = req.query.url;
 
-  const browser = await playwright.chromium.launch({headless: true});
-  const page = await browser.newPage();
-  await page.goto(url);
+      const browser = await Promise.resolve(bundledChromium.executablePath)
+          .then(
+              (executablePath) => {
+                if (!executablePath) {
+                  // local execution
+                  return chromium.launch({});
+                }
+                return chromium.launch({executablePath});
+              },
+          );
 
-  const title = await page.title();
-  const description = await page.$eval("meta[name=\"description\"]",
-      (el) => el.content);
-  const imageUrl = await page.$eval("meta[property=\"og:image\"]",
-      (el) => el.content);
+      const page = await browser.newPage();
+      await page.goto(url);
 
-  await browser.close();
+      const title = await page.title();
+      const description = await page.$eval("meta[name=\"description\"]",
+          (el) => el.content);
+      const imageUrl = await page.$eval("meta[property=\"og:image\"]",
+          (el) => el.content);
 
-  res.status(200).send({title, description, imageUrl});
-});
+      await browser.close();
+
+      res.status(200).send({title, description, imageUrl});
+    });
